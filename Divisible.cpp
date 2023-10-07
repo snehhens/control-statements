e962a382c0ace50d2edb4b9a40c10a4f975ded49
snehhens <theartroom12@gmail.com>
2023-10-07 09:03:15 +0530

    Divisible

--- Divisible.cpp
+++ Divisible.cpp
@@ -1,15 +1,40 @@
-
-
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  for (int i = 1; i <= 100; i++) {
-    if (i % 2 == 0) {
-      cout << i << endl;
-    }
+  int divisibleBy;
+
+  cout << "Enter a number to find which numbers from 1 to 100 are divisible by : ";
+  cin >> divisibleBy;
+
+// Switch statement checks which number the user entered and executes the appropriate code.
+  switch (divisibleBy) {
+    case 2: // If the user entered 2, the program will print all even numbers from 1 to 100.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    case 3: // If the user entered 3, the program will print all numbers from 1 to 100 that are divisible by 3.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    case 5: // If the user entered 5, the program will print all numbers from 1 to 100 that are divisible by 5.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 5 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    default:
+      cout << "Invalid input." << endl;
   }
 
   return 0;
 }
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,119 +1,39 @@
 #include <iostream>
-#include <vector>
 
-enum class Player { X, O, Empty };
-
-class TicTacToe {
-public:
-    TicTacToe() : currentPlayer(Player::X), board(3, std::vector<Player>(3, Player::Empty)) {}
-
-    void playGame() {
-        while (!isGameOver()) {
-            displayBoard();
-            makeMove();
-            switchPlayer();
-        }
-
-        displayResult();
-    }
-
-private:
-    Player currentPlayer;
-    std::vector<std::vector<Player>> board;
-
-    void displayBoard() const {
-        std::cout << "  0 1 2" << std::endl;
-        for (int row = 0; row < 3; ++row) {
-            std::cout << row << " ";
-            for (int col = 0; col < 3; ++col) {
-                switch (board[row][col]) {
-                    case Player::X:
-                        std::cout << "X ";
-                        break;
-                    case Player::O:
-                        std::cout << "O ";
-                        break;
-                    case Player::Empty:
-                        std::cout << ". ";
-                        break;
-                }
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
-
-    void makeMove() {
-        int row, col;
-        std::cout << "Player " << static_cast<char>(currentPlayer) << ", enter your move (row and column): ";
-        std::cin >> row >> col;
-
-        if (isValidMove(row, col)) {
-            board[row][col] = currentPlayer;
-        } else {
-            std::cout << "Invalid move. Try again." << std::endl;
-            makeMove();
-        }
-    }
-
-    bool isValidMove(int row, int col) const {
-        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == Player::Empty;
-    }
-
-    bool isGameOver() const {
-        return checkWin() || checkTie();
-    }
-
-    bool checkWin() const {
-        // Check rows and columns
-        for (int i = 0; i < 3; ++i) {
-            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
-                return true; // Check row
-            }
-            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
-                return true; // Check column
-            }
-        }
-
-        // Check diagonals
-        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
-            return true; // Check main diagonal
-        }
-        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
-            return true; // Check anti-diagonal
-        }
-
-        return false;
-    }
-
-    bool checkTie() const {
-        for (const auto& row : board) {
-            for (const auto& cell : row) {
-                if (cell == Player::Empty) {
-                    return false; // The board is not full
-                }
-            }
-        }
-        return true; // The board is full, and no player has won
-    }
-
-    void switchPlayer() {
-        currentPlayer = (currentPlayer == Player::X) ? Player::O : Player::X;
-    }
-
-    void displayResult() const {
-        displayBoard();
-        if (checkWin()) {
-            std::cout << "Player " << static_cast<char>(currentPlayer) << " wins!" << std::endl;
-        } else {
-            std::cout << "It's a tie!" << std::endl;
-        }
-    }
-};
+using namespace std;
 
 int main() {
-    TicTacToe game;
-    game.playGame();
+  int divisibleBy;
 
-    return 0;
+  cout << "Enter a number to find which numbers from 1 to 100 are divisible by : ";
+  cin >> divisibleBy;
+
+// Switch statement checks which number the user entered and executes the appropriate code.
+  switch (divisibleBy) {
+    case 2: // If the user entered 2, the program will print all even numbers from 1 to 100.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    case 3: // If the user entered 3, the program will print all numbers from 1 to 100 that are divisible by 3.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    case 5: // If the user entered 5, the program will print all numbers from 1 to 100 that are divisible by 5.
+      for (int i = 1; i <= 100; i++) {
+        if (i % 5 == 0) {
+          cout << i << endl;
+        }
+      }
+      break;
+    default:
+      cout << "Invalid input." << endl;
+  }
+
+  return 0;
 }
